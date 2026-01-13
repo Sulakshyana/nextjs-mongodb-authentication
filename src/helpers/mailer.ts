58be38e56a783d301fb54540596cf8a98dc862f9
swastika <swastikaghimire100@gmail.com>
2026-01-13 20:08:49 +0545

    feat: implement email service for verification and password reset tokens

---
+++ src/helpers/mailer.ts
@@ -0,0 +1,56 @@
+import nodemailer from "nodemailer";
+import User from "@/models/userModels";
+import bcrypt from "bcryptjs";
+
+export const sendEmail = async ({ email, emailTypes, userId }: any) => {
+  try {
+    //create hashed token
+    const hashedToken = await bcrypt.hash(userId.toString(), 10);
+
+    if (emailTypes === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000, // 1 hour
+      });
+    } else if (emailTypes === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000, // 1 hour
+      });
+    }
+    const host = process.env.MAILTRAP_SMTP_HOST;
+    const port = process.env.MAILTRAP_SMTP_PORT;
+
+    if (!host || !port) {
+      throw new Error("Missing SMTP env vars");
+    }
+
+    var transport = nodemailer.createTransport({
+      host: host,
+      port: Number(port),
+      auth: {
+        user: process.env.MAILTRAP_SMTP_USER,
+        pass: process.env.MAILTRAP_SMTP_PASS,
+      },
+    });
+    const mailOptions = {
+      from: "mail.taskManager@example.com",
+      to: email,
+      subject:
+        emailTypes === "VERIFY" ? "Verify Your Email" : "Reset Your Password",
+      html: `<p>click <a href="${
+        process.env.domain
+      }/verify-email?token=${hashedToken}">here</a> to ${
+        emailTypes === "VERIFY" ? "verify your email" : "reset your password"
+      }:</p>`,
+    };
+
+    const mailResponse = await transport.sendMail(mailOptions);
+    return mailResponse;
+  } catch (error) {
+    console.error(
+      "Email service failed. Make sure you have provided your mailTrap credentials on .env files"
+    );
+    console.error("Error:", error);
+  }
+};
